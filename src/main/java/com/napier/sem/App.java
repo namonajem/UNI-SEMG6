a0f1cca72190d2781a434124100d6db512e2f36a
namonajem <namonajem@gmail.com>
2019-01-30 11:58:17 +0000

    mongo / docker further integration

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,8 +9,9 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        // Connect to MongoDB
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
+        //MongoClient mongoClient = new MongoClient("localhost", 27000);
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
@@ -26,5 +27,7 @@ public class App
         // Check document in collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
+
+
     }
 }
