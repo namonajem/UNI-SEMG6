3e587a16ca94f4b965f5340c61d6c31ae8b157e7
40213185 <40213185@live.napier.ac.uk>
2019-03-19 22:53:19 +0000

    Added needed tests in comments in AppTest

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,87 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+}
+/*
+//printCountriesByPopTest()
+//printCountriesByPopTestEmpty()
+//printCountriesByPopTestContainsNull()
+
+//printCountriesInContinentByPopTest()
+//printCountriesInContinentByPopTestEmpty()
+//printCountriesInContinentByPopTestContainsNull()
+
+//printCountriesInRegionByPopTest()
+//printCountriesInRegionByPopTestEmpty()
+//printCountriesInRegionByPopTestContainsNull()
+
+//printTopCountriesByPopTest()
+//printTopCountriesByPopTestEmpty()
+//printTopCountriesByPopTestContainsNull()
+
+//printTopCountriesInContinentByPopTest()
+//printTopCountriesInContinentByPopTestEmpty()
+//printTopCountriesInContinentByPopTestContainsNull()
+
+//printTopCountriesInRegionByPopTest()
+//printTopCountriesInRegionByPopTestEmpty()
+//printTopCountriesInRegionByPopTestContainsNull()
+
+//printAllCitiesByPopTest()
+//printAllCitiesByPopTestEmpty()
+//printAllCitiesByPopTestContainsNull()
+
+
+//printAllCitiesInContinentByPopTest()
+//printAllCitiesInContinentByPopTestEmpty()
+//printAllCitiesInContinentByPopTestContainsNull()
+
+//printAllCitiesInRegionByPopTest()
+//printAllCitiesInRegionByPopTestEmpty()
+//printAllCitiesInRegionByPopTestContainsNull()
+
+//printAllCitiesInCountryByPopTest()
+//printAllCitiesInCountryByPopTestEmpty()
+//printAllCitiesInCountryByPopTestContainsNull()
+
+//printAllCitiesInDistrictByPopTest()
+//printAllCitiesInDistrictByPopTestEmpty()
+//printAllCitiesInDistrictByPopTestContainsNull()
+
+//printTopCitiesByPopTest()
+//printTopCitiesByPopTestEmpty()
+//printTopCitiesByPopTestContainsNull()
+
+//printTopCitiesInContinentByPopTest()
+//printTopCitiesInContinentByPopTestEmpty()
+//printTopCitiesInContinentByPopTestContainsNull()
+
+//printTopCitiesInRegionByPopTest()
+//printTopCitiesInRegionByPopTestEmpty()
+//printTopCitiesInRegionByPopTestContainsNull()
+
+//printTopCitiesInCountryByPopTest()
+//printTopCitiesInCountryByPopTestEmpty()
+//printTopCitiesInCountryByPopTestContainsNull()
+
+//printTopCitiesInDistrictByPopTest()
+//printTopCitiesInDistrictByPopTestEmpty()
+//printTopCitiesInDistrictByPopTestContainsNull()
+
+ */
