2e65158cf9e29157e3c669f8753ccb1d8a8bbc35
40213185 <40213185@live.napier.ac.uk>
2019-03-20 01:14:39 +0000

    removed unnecessary code

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest
         app = new App();
     }
 
-    //countries by pop tests -printCountriesByPop
+    //countries by pop tests
     @Test
     void printCountriesByPopTest(){
         ArrayList<Country> countries = new ArrayList<>();
@@ -57,15 +57,9 @@ public class AppTest
         ArrayList<Country> countries = new ArrayList<>();
 
         Country cntry = new Country();
-        Country cntry1 = new Country();
-        Country cntry2 = new Country();
 
         cntry.code ="CHL";cntry.name ="Chile";/*cntry.continent = Continent.toContinent("South America");*/cntry.region ="South America";cntry.surfaceArea = 756626.00f;cntry.indepYear =1810;cntry.population =15211000;cntry.lifeExpectancy =75.7;cntry.gnp =72949.00f;cntry.gnpOld =75780.00f;cntry.localName ="Chile";cntry.governmentForm ="Republic";cntry.headOfState ="Ricardo Lagos Escobar";cntry.capital =554;cntry.code2 ="CL";
         countries.add(cntry);
-        cntry2.code ="BHR";cntry2.name ="Bahrain";/*cntry.continent = Continent.toContinent("Asia");*/cntry2.region ="Middle East";cntry2.surfaceArea = 694.00f;cntry2.indepYear =1971;cntry2.population =617000;cntry2.lifeExpectancy =73.0f;cntry2.gnp =6366.00f;cntry2.gnpOld =6097.00f;cntry2.localName ="Al-Bahrayn";cntry2.governmentForm ="Monarchy (Emirate)";cntry2.headOfState ="Hamad ibn Isa al-Khalifa";cntry2.capital =149;cntry2.code2 ="BH";
-        countries.add(cntry2);
-        cntry1.code ="ATG";cntry1.name ="Antigua and Barbuda";/*cntry1.continent = Continent.toContinent(""North America"");*/cntry1.region ="Caribbean";cntry1.surfaceArea = 442.00f;cntry1.indepYear =1981;cntry1.population =68000;cntry1.lifeExpectancy =70.5f;cntry1.gnp =612.00f;cntry1.gnpOld =584.00f;cntry1.localName ="Antigua and Barbuda";cntry1.governmentForm ="Constitutional Monarchy";cntry1.headOfState ="Elisabeth II";cntry1.capital =63;cntry1.code2 ="AG";
-        countries.add(cntry1);
 
         app.printCountriesInContinentByPop(countries, "South America");
     }
@@ -91,15 +85,9 @@ public class AppTest
         ArrayList<Country> countries = new ArrayList<>();
 
         Country cntry = new Country();
-        Country cntry1 = new Country();
-        Country cntry2 = new Country();
 
         cntry.code ="CHL";cntry.name ="Chile";/*cntry.continent = Continent.toContinent("South America");*/cntry.region ="South America";cntry.surfaceArea = 756626.00f;cntry.indepYear =1810;cntry.population =15211000;cntry.lifeExpectancy =75.7;cntry.gnp =72949.00f;cntry.gnpOld =75780.00f;cntry.localName ="Chile";cntry.governmentForm ="Republic";cntry.headOfState ="Ricardo Lagos Escobar";cntry.capital =554;cntry.code2 ="CL";
         countries.add(cntry);
-        cntry2.code ="BHR";cntry2.name ="Bahrain";/*cntry.continent = Continent.toContinent("Asia");*/cntry2.region ="Middle East";cntry2.surfaceArea = 694.00f;cntry2.indepYear =1971;cntry2.population =617000;cntry2.lifeExpectancy =73.0f;cntry2.gnp =6366.00f;cntry2.gnpOld =6097.00f;cntry2.localName ="Al-Bahrayn";cntry2.governmentForm ="Monarchy (Emirate)";cntry2.headOfState ="Hamad ibn Isa al-Khalifa";cntry2.capital =149;cntry2.code2 ="BH";
-        countries.add(cntry2);
-        cntry1.code ="ATG";cntry1.name ="Antigua and Barbuda";/*cntry1.continent = Continent.toContinent(""North America"");*/cntry1.region ="Caribbean";cntry1.surfaceArea = 442.00f;cntry1.indepYear =1981;cntry1.population =68000;cntry1.lifeExpectancy =70.5f;cntry1.gnp =612.00f;cntry1.gnpOld =584.00f;cntry1.localName ="Antigua and Barbuda";cntry1.governmentForm ="Constitutional Monarchy";cntry1.headOfState ="Elisabeth II";cntry1.capital =63;cntry1.code2 ="AG";
-        countries.add(cntry1);
 
         app.printCountriesInRegionByPop(countries,"Middle East");
     }
@@ -124,23 +112,19 @@ public class AppTest
         ArrayList<Country> countries = new ArrayList<>();
 
         Country cntry = new Country();
-        Country cntry1 = new Country();
-        Country cntry2 = new Country();
 
         cntry.code ="CHL";cntry.name ="Chile";/*cntry.continent = Continent.toContinent("South America");*/cntry.region ="South America";cntry.surfaceArea = 756626.00f;cntry.indepYear =1810;cntry.population =15211000;cntry.lifeExpectancy =75.7;cntry.gnp =72949.00f;cntry.gnpOld =75780.00f;cntry.localName ="Chile";cntry.governmentForm ="Republic";cntry.headOfState ="Ricardo Lagos Escobar";cntry.capital =554;cntry.code2 ="CL";
         countries.add(cntry);
-        cntry2.code ="BHR";cntry2.name ="Bahrain";/*cntry.continent = Continent.toContinent("Asia");*/cntry2.region ="Middle East";cntry2.surfaceArea = 694.00f;cntry2.indepYear =1971;cntry2.population =617000;cntry2.lifeExpectancy =73.0f;cntry2.gnp =6366.00f;cntry2.gnpOld =6097.00f;cntry2.localName ="Al-Bahrayn";cntry2.governmentForm ="Monarchy (Emirate)";cntry2.headOfState ="Hamad ibn Isa al-Khalifa";cntry2.capital =149;cntry2.code2 ="BH";
-        countries.add(cntry2);
-        cntry1.code ="ATG";cntry1.name ="Antigua and Barbuda";/*cntry1.continent = Continent.toContinent(""North America"");*/cntry1.region ="Caribbean";cntry1.surfaceArea = 442.00f;cntry1.indepYear =1981;cntry1.population =68000;cntry1.lifeExpectancy =70.5f;cntry1.gnp =612.00f;cntry1.gnpOld =584.00f;cntry1.localName ="Antigua and Barbuda";cntry1.governmentForm ="Constitutional Monarchy";cntry1.headOfState ="Elisabeth II";cntry1.capital =63;cntry1.code2 ="AG";
-        countries.add(cntry1);
 
         app.printTopCountriesByPop(countries, 5);
     }
+
     @Test
     void printTopCountriesByPopTestEmpty(){
         ArrayList<Country> countries = new ArrayList<>();
         app.printTopCountriesByPop(countries, 5);
     }
+
     @Test
     void printTopCountriesByPopTestContainsNull(){
         ArrayList<Country> countries = new ArrayList<>();
@@ -155,23 +139,19 @@ public class AppTest
         ArrayList<Country> countries = new ArrayList<>();
 
         Country cntry = new Country();
-        Country cntry1 = new Country();
-        Country cntry2 = new Country();
 
         cntry.code ="CHL";cntry.name ="Chile";/*cntry.continent = Continent.toContinent("South America");*/cntry.region ="South America";cntry.surfaceArea = 756626.00f;cntry.indepYear =1810;cntry.population =15211000;cntry.lifeExpectancy =75.7;cntry.gnp =72949.00f;cntry.gnpOld =75780.00f;cntry.localName ="Chile";cntry.governmentForm ="Republic";cntry.headOfState ="Ricardo Lagos Escobar";cntry.capital =554;cntry.code2 ="CL";
         countries.add(cntry);
-        cntry2.code ="BHR";cntry2.name ="Bahrain";/*cntry.continent = Continent.toContinent("Asia");*/cntry2.region ="Middle East";cntry2.surfaceArea = 694.00f;cntry2.indepYear =1971;cntry2.population =617000;cntry2.lifeExpectancy =73.0f;cntry2.gnp =6366.00f;cntry2.gnpOld =6097.00f;cntry2.localName ="Al-Bahrayn";cntry2.governmentForm ="Monarchy (Emirate)";cntry2.headOfState ="Hamad ibn Isa al-Khalifa";cntry2.capital =149;cntry2.code2 ="BH";
-        countries.add(cntry2);
-        cntry1.code ="ATG";cntry1.name ="Antigua and Barbuda";/*cntry1.continent = Continent.toContinent(""North America"");*/cntry1.region ="Caribbean";cntry1.surfaceArea = 442.00f;cntry1.indepYear =1981;cntry1.population =68000;cntry1.lifeExpectancy =70.5f;cntry1.gnp =612.00f;cntry1.gnpOld =584.00f;cntry1.localName ="Antigua and Barbuda";cntry1.governmentForm ="Constitutional Monarchy";cntry1.headOfState ="Elisabeth II";cntry1.capital =63;cntry1.code2 ="AG";
-        countries.add(cntry1);
 
         app.printTopCountriesInContinentByPop(countries,"Asia", 5);
     }
+
     @Test
     void printTopCountriesInContinentByPopTestEmpty(){
         ArrayList<Country> countries = new ArrayList<>();
         app.printTopCountriesInContinentByPop(countries,"Asia", 5);
     }
+
     @Test
     void printTopCountriesInContinentByPopTestContainsNull(){
         ArrayList<Country> countries = new ArrayList<>();
@@ -186,23 +166,19 @@ public class AppTest
         ArrayList<Country> countries = new ArrayList<>();
 
         Country cntry = new Country();
-        Country cntry1 = new Country();
-        Country cntry2 = new Country();
 
         cntry.code ="CHL";cntry.name ="Chile";/*cntry.continent = Continent.toContinent("South America");*/cntry.region ="South America";cntry.surfaceArea = 756626.00f;cntry.indepYear =1810;cntry.population =15211000;cntry.lifeExpectancy =75.7;cntry.gnp =72949.00f;cntry.gnpOld =75780.00f;cntry.localName ="Chile";cntry.governmentForm ="Republic";cntry.headOfState ="Ricardo Lagos Escobar";cntry.capital =554;cntry.code2 ="CL";
         countries.add(cntry);
-        cntry2.code ="BHR";cntry2.name ="Bahrain";/*cntry.continent = Continent.toContinent("Asia");*/cntry2.region ="Middle East";cntry2.surfaceArea = 694.00f;cntry2.indepYear =1971;cntry2.population =617000;cntry2.lifeExpectancy =73.0f;cntry2.gnp =6366.00f;cntry2.gnpOld =6097.00f;cntry2.localName ="Al-Bahrayn";cntry2.governmentForm ="Monarchy (Emirate)";cntry2.headOfState ="Hamad ibn Isa al-Khalifa";cntry2.capital =149;cntry2.code2 ="BH";
-        countries.add(cntry2);
-        cntry1.code ="ATG";cntry1.name ="Antigua and Barbuda";/*cntry1.continent = Continent.toContinent(""North America"");*/cntry1.region ="Caribbean";cntry1.surfaceArea = 442.00f;cntry1.indepYear =1981;cntry1.population =68000;cntry1.lifeExpectancy =70.5f;cntry1.gnp =612.00f;cntry1.gnpOld =584.00f;cntry1.localName ="Antigua and Barbuda";cntry1.governmentForm ="Constitutional Monarchy";cntry1.headOfState ="Elisabeth II";cntry1.capital =63;cntry1.code2 ="AG";
-        countries.add(cntry1);
 
         app.printTopCountriesInRegionByPop(countries,"Carribean", 5);
     }
+
     @Test
     void printTopCountriesInRegionByPopTestEmpty(){
         ArrayList<Country> countries = new ArrayList<>();
         app.printTopCountriesInRegionByPop(countries,"Carribean", 5);
     }
+
     @Test
     void printTopCountriesInRegionByPopTestContainsNull(){
         ArrayList<Country> countries = new ArrayList<>();
@@ -218,10 +194,12 @@ public class AppTest
 
 
     }
+
     @Test
     void printAllCitiesByPopTestEmpty(){
         ArrayList<City> cities = new ArrayList<>();
     }
+
     @Test
     void printAllCitiesByPopTestContainsNull(){
         ArrayList<City> cities = new ArrayList<>();
