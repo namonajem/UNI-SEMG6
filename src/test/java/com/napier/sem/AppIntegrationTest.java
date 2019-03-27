7a70c8dd902b7f966312b22cc597f2bc096a80b3
Pablo <pablo_f.i@hotmail.com>
2019-03-27 11:39:10 +0000

    AppIntegrationTest added and travis updated

---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060");
+    }
+
+    @Test
+    void testGetCountry()
+    {
+        Country country = app.getCountryByCode("ESP");
+        assertEquals(country.code, "ESP");
+        assertEquals(country.name, "Spain");
+        assertEquals(country.capital, 653);
+    }
+}
