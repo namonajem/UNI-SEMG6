3144d5d276a775007764ab27814dab9d388a1d88
Pablo <pablo_f.i@hotmail.com>
2019-05-05 21:03:12 +0100

    Unit testing removed

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -12,57 +12,5 @@ public class AppTest {
         app = new App();
     }
 
-    @Test
-    void printCountriesReportTestNull() {
-        app.printCountriesReport(null, "Test report");
-    }
-
-    @Test
-    void printCountriesReportTestEmpty() {
-        List<Country> countries = new ArrayList<>();
-        app.printCountriesReport(countries, "Test report");
-    }
-
-    @Test
-    void printCountriesReportTestContainsNull() {
-        List<Country> countries = new ArrayList<>();
-        countries.add(null);
-        app.printCountriesReport(countries, "Test report");
-    }
-
-    @Test
-    void printCitiesReportTestNull() {
-        app.printCitiesReport(null, "Test report");
-    }
-
-    @Test
-    void printCitiesReportTestEmpty() {
-        List<City> cities = new ArrayList<>();
-        app.printCitiesReport(cities, "Test report");
-    }
-
-    @Test
-    void printCitiesReportTestContainsNull() {
-        List<City> cities = new ArrayList<>();
-        cities.add(null);
-        app.printCitiesReport(cities, "Test report");
-    }
-
-    @Test
-    void printCapitalCitiesReportTestNull() {
-        app.printCapitalCitiesReport(null, "Test report");
-    }
-
-    @Test
-    void printCapitalCitiesReportTestEmpty() {
-        List<City> cities = new ArrayList<>();
-        app.printCapitalCitiesReport(cities, "Test report");
-    }
-
-    @Test
-    void printCapitalCitiesReportTestContainsNull() {
-        List<City> cities = new ArrayList<>();
-        cities.add(null);
-        app.printCapitalCitiesReport(cities, "Test report");
-    }
+    
 }
